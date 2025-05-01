7416784cda9ae5db6f7bf6c6ec7977382bdf88d8
Natan <natan.nh20@gmail.com>
2025-05-01 19:47:11 -0300

    base

---
+++ src/main/java/com/natan/demo/NatanwbflxApplication.java
@@ -0,0 +1,13 @@
+package com.natan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatanwbflxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatanwbflxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natan/demo/NatanwbflxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatanwbflxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
